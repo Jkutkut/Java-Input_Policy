5ac9c2ddc68290bdb2218b13c9f0dedb296c9467
Jkutkut <jorgeregonzalez@gmail.com>
2022-10-27 12:20:00 +0200

    datePolicy tests fixed

--- src/jkutkut/inputPolicy/model/DatePolicy.java
+++ src/jkutkut/inputPolicy/model/DatePolicy.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 import java.util.function.BiFunction;
 import java.util.function.Predicate;
 
-class BirthdayPolicy extends InputPolicy {
+public class DatePolicy extends InputPolicy {
     protected static final int DAY_IDX = 0;
     protected static final int MONTH_IDX = 1;
     protected static final int YEAR_IDX = 2;
@@ -26,16 +26,17 @@ class BirthdayPolicy extends InputPolicy {
 
     protected static final Predicate<String> validDateFormat = d -> {
         if (d == null) return false;
-        return "\\d{2}/\\d{2}/\\d{4}".matches(d);
+        return d.matches("\\d{1,2}/\\d{1,2}/\\d{4}");
     };
 
     protected static final Predicate<String> validDateIntegers = d -> {
         if (d == null) return false;
         String[] date = d.split("/");
+        if (date.length != 3) return false;
         try {
-            Integer.parseInt(date[0]);
-            Integer.parseInt(date[1]);
-            Integer.parseInt(date[2]);
+            Integer.parseInt(date[DAY_IDX]);
+            Integer.parseInt(date[MONTH_IDX]);
+            Integer.parseInt(date[YEAR_IDX]);
         } catch (NumberFormatException e) {
             return false;
         }
@@ -45,6 +46,7 @@ class BirthdayPolicy extends InputPolicy {
     protected static final Predicate<String> validDate = d -> {
         if (d == null) return false;
         String[] date = d.split("/");
+        if (date.length != 3) return false;
         try {
             int day = Integer.parseInt(date[DAY_IDX]);
             int month = Integer.parseInt(date[MONTH_IDX]);
@@ -58,15 +60,15 @@ class BirthdayPolicy extends InputPolicy {
         return true;
     };
 
-    public BirthdayPolicy() {
+    public DatePolicy() {
         super();
         addTests();
     }
 
     private void addTests() {
         addTest(Objects::nonNull, "Date cannot be null");
-        addTest(validDateFormat, "Date must be in the format dd/mm/yyyy");
-        addTest(validDateIntegers, "Date must be in the format dd/mm/yyyy"); // Redundant
+        addTest(validDateFormat, "Date must be in the format dd/mm/yyyy (regex not met)");
+        addTest(validDateIntegers, "Date must be in the format dd/mm/yyyy (values not correct)");
         addTest(validDate, "Date is not valid.");
     }
 }
